193f604896684a0814cb3786e7b380d51cff97e8
XkotikZ <ShikEgorKotik@yandex.ru>
2022-12-14 13:18:04 +0500

    l10 +

--- l10.c
+++ l10.c
@@ -2,12 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
+void fail() {
+    printf("Memory allocation failure!\n");
+    exit(EXIT_FAILURE);
+}
+
 void fill(int n, int m, float a[n][m]) {
     for (int i = 0; i < n; i += 1) {
         for (int j = 0; j < m; j += 1) {
             a[i][j] = rand() % 101 - 50;
         }
     }
+}
+
+void fill_new(int n, int m, float** a) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            a[i][j] = rand() % 101 - 50;
 }
 
 void zeros(int n, int m, int a[n][m]) {
@@ -38,6 +49,16 @@ void print_array_float(int n, int m, float a[n][m]){
     printf("\n");
 }
 
+void print_array_float_new(int n, int m, float** a) {
+    for (int i = 0; i < n; i += 1) {
+        for (int j = 0; j < m; j += 1) {
+            printf("%8.3f", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 float max_nums(int n, int m, float a[n][m]) {
     float M = 0;
     for (int i = 0; i < n; i += 1) {
@@ -51,7 +72,21 @@ float max_nums(int n, int m, float a[n][m]) {
     return M;
 }
 
-void division_preparation(int n, int m, float M_num, float a[n][m], int pre_a[n][m]) {
+float max_nums_new(int n, int m, float** a) {
+    float M = 0;
+    for (int i = 0; i < n; i += 1) {
+        for (int j = 0; j < m; j += 1) {
+            if (M < abs(a[i][j])) {
+                M = abs(a[i][j]);
+            }
+        }
+    }
+
+    return M;
+}
+
+// void division_preparation(int n, int m, float M_num, float a[n][m], int pre_a[n][m]) {
+void division_preparation_new(int n, int m, float M_num, float** a, int pre_a[n][m]) {
     for (int i = 0; i < n; i += 1) {
         for (int j = 0; j < m; j += 1) {
             if (i == j) {
@@ -99,6 +134,16 @@ void division(int n, int m, float M, float a[n][m], int pre_a[n][m]) {
     }
 }
 
+void division_new(int n, int m, float M, float** a, int pre_a[n][m]) {
+    for (int i = 0; i < n; i += 1) {
+        for (int j = 0; j < m; j += 1) {
+            if (pre_a[i][j]) {
+                a[i][j] /= M;
+            }
+        }
+    }
+}
+
 int main() {
     srand(time(NULL));
 
@@ -110,24 +155,49 @@ int main() {
     printf("Enter m -> ");
     scanf("%i", &m);
 
-    float A[n][m];
-    fill(n, m, A);
-    printf("A := \n");
-    print_array_float(n, m, A);
+    // float A[n][m];
+    // fill(n, m, A);
+    // printf("A := \n");
+    // print_array_float(n, m, A);
 
-    float M_num = max_nums(n, m, A);
+    // new code
+    float **a = (float **)malloc(n * sizeof(float *));
+    if (!a)
+        fail();
+    // int i, j;
+    for (int i = 0; i < n; i++) {
+        a[i] = (float *)malloc(m * sizeof(float));
+        if (!a[i])
+            fail();
+    }
+
+    fill_new(n, m, a);
+
+    printf("a := \n");
+    print_array_float_new(n, m, a);
+
+
+    // float M_num = max_nums(n, m, A);
+    float M_num = max_nums_new(n, m, a);
     printf("M = %f \n", M_num);
 
     int pre_A[n][m];
     zeros(n, m, pre_A);
-    division_preparation(n, m, M_num, A, pre_A);
+    // division_preparation(n, m, M_num, A, pre_A);
+    division_preparation_new(n, m, M_num, a, pre_A);
     printf("pre A := \n");
     print_array_int(n, m, pre_A);
 
-    division(n, m, M_num, A, pre_A);
+    // division(n, m, M_num, A, pre_A);
+    division_new(n, m, M_num, a, pre_A);
 
-    printf("del A := \n");
-    print_array_float(n, m, A);
+    printf("del a := \n");
+    // print_array_float(n, m, A);
+    print_array_float_new(n, m, a);
+
+    for (int i = 0; i < n; i++)
+        free(a[i]);
+    free(a);
 
     return 0;
 }

bbe90f0cfe2e4390a21cd54d6f91df77d6d0b2a7
XkotikZ <ShikEgorKotik@yandex.ru>
2023-01-05 11:31:23 +0500

    l9 fix

--- l9.c
+++ l9.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 
+#include <termios.h>
+#include <unistd.h>
+
+
+int mygetch()
+{
+    struct termios oldt, newt;
+    int c;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    c = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return c;
+}
 
 int max_len(char *p, char *delim) {
     int max_l = 0, m;
@@ -24,17 +42,17 @@ int max_counter(int max_l, char *p, char *delim) {
 
     int l;
     l = strlen(p);
-    printf(" l = %i, %s \n", l, p);
+    // printf(" l = %i, %s \n", l, p);
     array_len_counter[l] += 1;
     while ((p = strtok(NULL, delim))) {
         l = strlen(p);
-        printf(" l = %i, %s \n", l, p);
+        // printf(" l = %i, %s \n", l, p);
         array_len_counter[l] += 1;
     }
 
-    for (int i = 0; i < max_l; i += 1) {
-        printf("%i-%i \n", i, array_len_counter[i]);
-    }
+    // for (int i = 0; i < max_l; i += 1) {
+    //     printf("%i-%i \n", i, array_len_counter[i]);
+    // }
 
     int max_count = 0;
     int max_count_num = 0;
@@ -48,6 +66,8 @@ int max_counter(int max_l, char *p, char *delim) {
 }
 
 void print_str(int max_count, char *p, char *delim) {
+    printf("\nout:\n");
+
     if (strlen(p) != max_count) {
         printf("%s ", p);
     }
@@ -61,21 +81,51 @@ void print_str(int max_count, char *p, char *delim) {
 
 int main()
 {
-    char* str = (char *)malloc(sizeof(char) * 100);
-    printf("%s", "Enter string: ");
-    fgets(str, 100, stdin);
+    // char* str = (char *)malloc(sizeof(char) * 100);
+    // printf("%s", "Enter string: ");
+    // fgets(str, 100, stdin);
+
+    printf("in:\n");
+    int i = 0, max_i = 100;
+    char* str = (char *)malloc(sizeof(char) * max_i);
+    int c;
+    while (true) {
+        c = mygetch();
+        if (c == 10) {
+            break;
+        }
+        // printf("", c);
+        printf("%c", tolower(c));
+        str[i] = (char)c;
+        i += 1;
+
+        if (max_i - i == 5) {
+            char* str_swap = (char *)malloc(sizeof(char) * max_i);
+            for (int j = 0; j < i; j += 1) {
+                str_swap[j] = str[j];
+            }
+        max_i += 10;
+        str = (char *)malloc(sizeof(char) * max_i);
+        }
+        // printf("%i | ", c);
+    }
+
     str = strtok(str, ",\n");
 
-    int str_len = strlen(str);
-    printf("%s, %i  \n", str, str_len);
-    char str1[str_len], str2[str_len], str3[str_len];
+    int str_len = i;
+    // int str_len = strlen(str);
+    // printf("%s, %i  \n", str, str_len);
+    // char str1[str_len], str2[str_len], str3[str_len];
+    char* str1 = (char *)malloc(sizeof(char) * str_len);
+    char* str2 = (char *)malloc(sizeof(char) * str_len);
+    char* str3 = (char *)malloc(sizeof(char) * str_len);
     for (int i = 0; i < str_len; i += 1) {
         str1[i] = str[i];
         str2[i] = str[i];
         str3[i] = str[i];
     }
 
-    printf("%s, %li \n", str1, strlen(str1));
+    // printf("%s, %li \n", str1, strlen(str1));
 
     // char str1[] = "Vicariously I live while the whole world"; // Vicariously I live the
     // char str2[] = "Vicariously I live while the whole world";
@@ -89,7 +139,7 @@ int main()
 
     p = strtok(str2, delim);
     int max_count = max_counter(max_l, p, delim);
-    printf("max_count = %i \n", max_count);
+    // printf("max_count = %i \n", max_count);
 
     p = strtok(str3, delim);
     print_str(max_count, p, delim);
